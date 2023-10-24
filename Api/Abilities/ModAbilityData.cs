ff743addc70238c414086856bf468f54f10d99f4
GrahamKracker <gjguzynski@gmail.com>
2023-10-24 14:19:25 -0400

    Added static references for ingame scriptable objects, added more wip world event stuff, redid event system, started documenting all of the properties in ScriptableObjectModContent.cs classes

--- Api/Abilities/ModAbilityData.cs
+++ Api/Abilities/ModAbilityData.cs
@@ -3,6 +3,8 @@ using Abyss.Events;
 using Abyss.Utilities;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Localization;
 
 namespace Abyss.Api.Abilities;
 
@@ -34,12 +36,35 @@ public abstract class ModAbilityData<T> : ScriptableObjectModContent<AbilityData
     public virtual bool AutoUnlock => true;
 
     /// <summary>
-    /// Unlocks this ability ingame, <see cref="GameManager.PlayerAbilities"/> needs to not be null
+    /// Unlocks this ability ingame, using the default implementation, <see cref="GameManager.PlayerAbilities"/> needs to not be null
     /// </summary>
-    public void Unlock()
+    public virtual void Unlock()
     {
         GameManager.Instance.PlayerAbilities.UnlockAbility(Item.name.ToLower());
     }
+
+    public VibrationData primaryVibration;
+    public VibrationData secondaryVibration;
+    public AssetReference castSFX;
+
+    [Header("Dependent Item Configuration")]
+    public ItemData[] linkedItems;
+
+    public ItemSubtype linkedItemSubtype;
+    public bool allowDamagedItems;
+    public bool allowExhaustedItems;
+    [Header("Timings")] public float duration;
+    public float cooldown;
+    public float castTime;
+    [Header("Behaviour")] public bool isContinuous;
+    public bool deactivateOnInputLayerChanged;
+    public bool requiresAbilityFocus;
+    public bool persistAbilityToggle;
+    public bool canFailCast;
+    public bool showsCounter;
+    public bool allowExitAction;
+    public ActionLayer exitActionLayer;
+    public float sfxRepeatThreshold;
 
     /// <inheritdoc />
     public override void Register()
@@ -50,24 +75,30 @@ public abstract class ModAbilityData<T> : ScriptableObjectModContent<AbilityData
         Item.nameKey = LocalizationManager.CreateLocalizedString(Id, DisplayName);
         Item.descriptionKey = LocalizationManager.CreateLocalizedString(Id + "_Description", Description);
         Item.icon = IconImage;
+        Item.primaryVibration = primaryVibration;
+        Item.secondaryVibration = secondaryVibration;
+        Item.castSFX = castSFX;
+        Item.linkedItems = linkedItems;
+        Item.linkedItemSubtype = linkedItemSubtype;
+        Item.allowDamagedItems = allowDamagedItems;
+        Item.allowExhaustedItems = allowExhaustedItems;
+        Item.duration = duration;
+        Item.cooldown = cooldown;
+        Item.castTime = castTime;
+        Item.isContinuous = isContinuous;
+        Item.deactivateOnInputLayerChanged = deactivateOnInputLayerChanged;
+        Item.requiresAbilityFocus = requiresAbilityFocus;
+        Item.persistAbilityToggle = persistAbilityToggle;
+        Item.canFailCast = canFailCast;
+        Item.showsCounter = showsCounter;
+        Item.allowExitAction = allowExitAction;
+        Item.exitActionLayer = exitActionLayer;
+        Item.sfxRepeatThreshold = sfxRepeatThreshold;
+
+
 
         Edit(Item);
 
-        /*AbyssEvents.OnGameSceneLoaded.AddListener(() =>
-        {
-            try
-            {
-                GameManager.Instance.PlayerAbilities.RegisterAbility(Item, Ability);
-                if (AutoUnlock)
-                {
-                    Unlock();
-                }
-            }
-            catch (Exception e)
-            {
-                AbyssLogger.Error("Error registering custom ability: \n" + e);
-            }
-        });*/
         ApplicationEvents.Instance.OnGameLoaded += () =>
         {
             try
